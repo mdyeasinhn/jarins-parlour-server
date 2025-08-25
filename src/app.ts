997a5233e85a3538d6b410d29c1bc8bbc9c3dbdf
mdyeasinhn <mdyeasinhn@gmail.com>
2025-08-25 22:35:46 +0600

    updateing server and app ts file

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,31 @@
+import cookieParser from 'cookie-parser';
 import express, { Request, Response } from "express";
-
+import cors from "cors";
+import router from "./routes";
 const app = express();
+
+//parsers
+app.use(express.json());
+app.use(cookieParser());
+
+app.use(cors());
+// Application routes
+app.use('/api', router);
 
 app.get('/', (req: Request, res: Response) => {
     res.send({
         status: true,
         message: "Server live⚡"
+    })
+});
+
+// Global Error Handler
+// app.use(globalErrorHandler)
+
+app.use('*', (req: Request, res: Response) => {
+    res.status(404).json({
+        success: false,
+        message: 'Route not found',
     })
 })
 
--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,34 @@
+/* eslint-disable no-console */
 import mongoose from "mongoose";
 import app from "./app";
 import config from "./config";
+import { Server } from 'http';
 
-async function server() {
+let server: Server;
+async function main() {
     try {
         await mongoose.connect(config.database_url as string);
-        app.listen(config.port, () => {
-            // eslint-disable-next-line no-console
+        server = app.listen(config.port, () => {
             console.log(`Server running on port ${config.port} 🏃`)
         })
     } catch (error) {
-        // eslint-disable-next-line no-console
         console.log(error)
     }
 }
-server();
+main();
+
+// trun of server
+process.on('unhandledRejection', () => {
+    console.log(`😈 unahandledRejection is detected , shutting down ...`);
+    if (server) {
+        server.close(() => {
+            process.exit(1);
+        });
+    }
+    process.exit(1);
+});
+
+process.on('uncaughtException', () => {
+    console.log(`😈 uncaughtException is detected , shutting down ...`);
+    process.exit(1);
+});
