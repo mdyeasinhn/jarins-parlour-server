06d338fe92d5565ae2cab817e955c84cc1d36291
mdyeasinhn <mdyeasinhn@gmail.com>
2025-09-06 21:04:25 +0600

    review model complete

---
+++ src/module/review/review.model.ts
@@ -0,0 +1,33 @@
+import { Schema, model } from "mongoose";
+import { IReview } from "./review.interface";
+
+const reviewSchema = new Schema<IReview>(
+  {
+    userId: {
+      type: Schema.Types.ObjectId,
+      ref: "User",      // Reference to User collection
+      required: true,
+    },
+    serviceId: {
+      type: Schema.Types.ObjectId,
+      ref: "Service",   // Reference to Service collection
+      required: true,
+    },
+    rating: {
+      type: Number,
+      required: true,
+      min: 1,
+      max: 5,
+    },
+    comment: {
+      type: String,
+      trim: true,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+    },
+  },
+);
+
+export const Review = model<IReview>("Review", reviewSchema);
